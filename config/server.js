db0cf226b5c8e024136441617ba7787bceba8710
Andre Castellanos <47199821+acastellanos95@users.noreply.github.com>
2021-10-07 13:54:44 -0500

    created strapi minimal backend with npx

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: process.env.HOST,
+  port: process.env.PORT,
+  admin: {
+    auth: {
+      secret: process.env.ADMIN_JWT_SECRET,
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '63ac65a6-2f61-4e82-9756-d802bfb723e7'
+};
